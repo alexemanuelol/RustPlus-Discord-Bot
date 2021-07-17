b438e958284c013e4c259491c687b435ee02ce11
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2021-07-18 01:35:06 +0200

    Add command getTime

---
+++ src/commands/getTime.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const RustPlus = require("rustplus.js");
+
+module.exports = {
+	name: "getTime",
+	description: "Get the current in game time.",
+	execute(message, args, bot, rustplus) {
+        if (args.length != 0)
+        {
+            console.log("ERROR: No arguments required.");
+            message.reply("ERROR: No arguments required.");
+            return false;
+        }
+
+        var str = "";
+
+        rustplus.getTime((msg) => {
+            let time = msg["response"]["time"];
+            str += "**Current time:** " + time["time"] + "\n";
+            str += "**Total daylight (minutes):** " + time["dayLengthMinutes"] + "\n";
+            str += "**Sunrise:** " + time["sunrise"] + "\n";
+            str += "**Sunset:** " + time["sunset"];
+
+            console.log(str);
+            message.reply(str);
+        });
+
+        return true;
+	},
+};
