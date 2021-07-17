343e979a3aed0e8e0ac2f85b453dfe4e77c5a2a8
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2021-07-18 01:34:01 +0200

    Add command turnOff

---
+++ src/commands/turnOff.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const RustPlus = require("rustplus.js");
+const fs = require("fs");
+
+module.exports = {
+	name: "turnOff",
+	description: "Turn off a Smart Switch.",
+	execute(message, args, bot, rustplus) {
+        if (args.length === 0)
+        {
+            console.log("ERROR: At least 1 argument is required. Example: !turnOff @name/id");
+            message.reply("ERROR: At least 1 argument is required. Example: !turnOff @name/id");
+            return false;
+        }
+
+        /* Read the devices.json file. */
+        fs.readFile("./devices.json", (err, data) => {
+            if (err) throw err;
+            let devices = JSON.parse(data);
+
+            for (let arg of args)
+            {
+                if (devices.hasOwnProperty(arg))
+                {
+                    rustplus.turnSmartSwitchOff(parseInt(devices[arg]), (msg) => {
+                        console.log("turnSmartSwitchOff response message: " + JSON.stringify(msg));
+                        message.reply("'" + arg + "' was turned off.");
+                    });
+                }
+                else
+                {
+                    rustplus.turnSmartSwitchOff(parseInt(arg), (msg) => {
+                        console.log("turnSmartSwitchOff response message: " + JSON.stringify(msg));
+                        message.reply("'" + arg + "' was turned off.");
+                    });
+                }
+            }
+        });
+
+        return true;
+	},
+};
