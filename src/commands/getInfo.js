9db8676c9ea18b9205316a48c6f6f3b550f7132e
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2021-07-18 01:35:52 +0200

    Add command getInfo

---
+++ src/commands/getInfo.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const RustPlus = require("rustplus.js");
+
+module.exports = {
+	name: "getInfo",
+	description: "Get info about the Rust Server.",
+	execute(message, args, bot, rustplus) {
+        if (args.length != 0)
+        {
+            console.log("ERROR: No arguments required.");
+            message.reply("ERROR: No arguments required.");
+            return false;
+        }
+
+        var str = "";
+
+        rustplus.getInfo((msg) => {
+            let info = msg["response"]["info"];
+            str += "**Server name:** " + info["name"] + "\n";
+            str += "**URL:** <" + info["url"] + ">\n";
+            str += "**Map:** " + info["map"] + "\n";
+            str += "**Map Size:** " + info["mapSize"] + "\n";
+            str += "**Wipe time:** " + info["wipeTime"] + "\n";
+            str += "**Online players:** (" + info["players"] + "/" + info["maxPlayers"] + ")\n";
+            str += "**Queued Players:** " + info["queuedPlayers"] + "\n";
+            str += "**Seed:** " + info["seed"] + "\n";
+            str += "**Salt:** " + info["salt"] + "\n";
+
+            console.log(str);
+            message.reply(str);
+        });
+
+        return true;
+	},
+};
