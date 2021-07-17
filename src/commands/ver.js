c532771c3259175520f2e65ac16ab5cbdd256ff8
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2021-07-18 01:28:31 +0200

    Created a version.json file to store bot version

--- src/commands/ver.js
+++ src/commands/ver.js
@@ -1,10 +1,12 @@
 const Discord = require("discord.js");
+const RustPlus = require("rustplus.js");
+const { version } = require("./../version.json");
 
 module.exports = {
-	name: 'ver',
-	description: 'Obtain the bot version.',
-	execute(message, args) {
-		//message.channel.send('Pong.');
-        message.reply("0.0.1")
+	name: "ver",
+	description: "Obtain the bot version.",
+	execute(message, args, bot, rustplus) {
+        console.log(version);
+        message.reply(version);
 	},
 };
