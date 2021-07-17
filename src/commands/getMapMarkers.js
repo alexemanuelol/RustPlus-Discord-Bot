865e564cfac8cfbf006bdeeb53b5976985937965
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2021-07-18 01:37:07 +0200

    Add command getMapMarkers

---
+++ src/commands/getMapMarkers.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const RustPlus = require("rustplus.js");
+
+module.exports = {
+	name: "getMapMarkers",
+	description: "Get map markers, such as vending machines and cargo/heli.",
+	execute(message, args, bot, rustplus) {
+        if (args.length != 0)
+        {
+            console.log("ERROR: No arguments required.");
+            message.reply("ERROR: No arguments required.");
+            return false;
+        }
+
+        var str = "";
+
+        rustplus.getMapMarkers((msg) => {
+            console.log("TBD");
+            //console.log(msg["response"]["mapMarkers"]);
+
+            /* TODO: Find a convenient way to display the information. */
+        });
+
+        return true;
+	},
+};
