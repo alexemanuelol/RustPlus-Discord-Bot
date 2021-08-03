c922f9b51f87493d142c132923b813685d8c6d52
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2021-08-03 20:50:23 +0200

    Changed the config file structure

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,11 @@
-const Config = require("./../config.json");
 const Tools = require("./../tools/tools.js");
 
 module.exports = {
     name: "help",
     description: "Displays a help message.",
     execute(author, message, channel, args, discordBot, rustplus) {
+        let config = Tools.readJSON("./config.json");
+
         let str = "";
 
         for (let command of discordBot.commands.keys()) {
@@ -12,7 +13,7 @@ module.exports = {
         }
 
         let title = "Help Information";
-        let description = "Command prefix: **" + Config.prefix + "**";
+        let description = "Command prefix: **" + config.general.prefix + "**";
         Tools.print(title, description + "\n**Available Commands:**\n" + str);
         Tools.sendEmbed(channel, title, description, ["**Available Commands:**", str]);
 
