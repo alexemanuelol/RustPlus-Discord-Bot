686399ddd163529ac4405e800d383e941da264d8
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2021-08-11 21:00:11 +0200

    Add getSettings command

---
+++ commands/getSettings.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+
+const Main = require("./../rustplusDiscordBot.js");
+const Tools = require("./../tools/tools.js");
+
+module.exports = {
+    name: "getSettings",
+    description: "Get the current config settings.",
+    execute(author, message, channel, args, discordBot, rustplus) {
+        /* Read the config.json file. */
+        let config = Tools.readJSON("./config.json");
+
+        /* Verify that there are no arguments. */
+        if (args.length != 0) {
+            Tools.print("ERROR", "No arguments required.", channel);
+            return false;
+        }
+
+        let title = "Bot Settings";
+        let description = "";
+
+        description += "Prefix: **" + config.general.prefix + "**\n\n";
+        description += "Notifications: **" + config.notifications.enabled + "**\n";
+        description += "In-Game Notifications: **" + config.notifications.inGame + "**\n";
+        description += "Cargo Ship Notifications: **" + config.notifications.cargoShip + "**\n";
+        description += "Chinook 47 Notifications: **" + config.notifications.chinook47 + "**\n";
+        description += "Crate Notifications: **" + config.notifications.crate + "**\n";
+        description += "Explosion Notifications: **" + config.notifications.explosion + "**\n\n";
+        description += "Alarm Notifications: **" + config.alarms.enabled + "**\n";
+        description += "In-Game Alarm Notifications : **" + config.alarms.inGame + "**\n\n";
+        description += "Storage Monitor Notifications: **" + config.storageMonitors.enabled + "**";
+
+        Tools.print(title, description, channel);
+
+        return true;
+    },
+};
